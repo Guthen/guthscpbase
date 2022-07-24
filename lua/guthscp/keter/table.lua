6b62a38171b8b32a498336f935363df5a11ab37b
Guthen <33220603+Guthen@users.noreply.github.com>
2022-07-24 17:53:37 +0200

    [documented] `guthscp.table` functions

--- lua/guthscp/keter/table.lua
+++ lua/guthscp/keter/table.lua
@@ -1,5 +1,13 @@
 guthscp.table = guthscp.table or {}
 
+--[[ 
+    @function guthscp.table.merge
+        | description: merges a table's values into another
+        | params:
+            to_tbl: <table> target table
+            from_tbl: <table> table to merge into the target
+        | return: <table> to_tbl
+]]
 function guthscp.table.merge( to_tbl, from_tbl )
     for k, v in pairs( from_tbl ) do
         if not ( from_tbl[k] == nil ) then
@@ -10,6 +18,13 @@ function guthscp.table.merge( to_tbl, from_tbl )
     return to_tbl
 end
 
+--[[ 
+    @function guthscp.table.reverse
+        | description: reverses the values and the keys of the given table; keys becoming values and vice-versa
+        | params:
+            tbl: const <table> table to reverse
+        | return: <table> new_tbl
+]]
 function guthscp.table.reverse( tbl )
     local new_tbl = {}
 
@@ -20,6 +35,13 @@ function guthscp.table.reverse( tbl )
     return new_tbl
 end
 
+--[[ 
+    @function guthscp.table.create_set
+        | description: creates a set table (https://www.lua.org/pil/11.5.html) out of the given table's values
+        | params:
+            tbl: const <table> sequential table to use
+        | return: <table> new_tbl
+]]
 function guthscp.table.create_set( tbl )
     local new_tbl = {}
 
@@ -30,6 +52,13 @@ function guthscp.table.create_set( tbl )
     return new_tbl
 end
 
+--[[ 
+    @function guthscp.table.rehash
+        | description: re-hashs the given table; creates a sequential table out of a (probably) non-sequential one
+        | params:
+            tbl: const <table> table to re-hash
+        | return: <table> new_tbl
+]]
 function guthscp.table.rehash( tbl )
     local new_tbl = {}
 
