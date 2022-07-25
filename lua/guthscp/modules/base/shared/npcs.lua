fc4056b8901466d8ebf3cee82f20fa939e625255
Guthen <33220603+Guthen@users.noreply.github.com>
2022-07-25 16:10:23 +0200

    [added] NPCs cache

---
+++ lua/guthscp/modules/base/shared/npcs.lua
@@ -0,0 +1,21 @@
+local npcs = {}
+hook.Add( "OnEntityCreated", "guthscp:retrieve_npcs", function( ent )
+    if not IsValid( ent ) or ( not ent:IsNPC() and not ent:IsNextBot() ) then return end
+
+    npcs[#npcs + 1] = ent
+end )
+
+hook.Add( "EntityRemoved", "guthscp:remove_npcs", function( ent )
+    if not IsValid( ent ) or ( not ent:IsNPC() and not ent:IsNextBot() ) then return end
+
+    for i, v in ipairs( npcs ) do
+        if v == ent then
+            table.remove( npcs, i )
+            break
+        end
+    end
+end )
+
+function guthscp.get_npcs()
+    return npcs
+end
