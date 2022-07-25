5aebab59a32569c1edc18af9e634a98e7ccd009d
Guthen <33220603+Guthen@users.noreply.github.com>
2022-07-25 19:26:04 +0200

    [doc] `base/shared/base.lua` functions

--- lua/guthscp/modules/base/shared/base.lua
+++ lua/guthscp/modules/base/shared/base.lua
@@ -1,4 +1,11 @@
 
+--[[ 
+    @function guthscp.is_scp
+        | description: check if a given player is in a SCP team
+        | params:
+            ply: <Player>
+        | return: <bool> is_scp
+]]
 function guthscp.is_scp( ply )
     if not ply:IsPlayer() then return false end
 
@@ -6,6 +13,11 @@ function guthscp.is_scp( ply )
     return teams[guthscp.get_team_keyname( isnumber( ply ) and ply or ply:Team() )] or false
 end
 
+--[[ 
+    @function guthscp.get_scps
+        | description: get all players currently in a SCP team, based on the server's configuration
+        | return: <table[Player]> players
+]]
 function guthscp.get_scps()
     local teams, players = guthscp.configs.base.scp_teams, {}
 
@@ -24,6 +36,11 @@ function guthscp.get_scps()
 end
 
 --  teams
+--[[ 
+    @function guthscp.cache_teams_keynames
+        | description: cache all teams keynames (global Lua variable names) and their IDs values by looping over `_G`; 
+                       used internally by @`guthscp.get_teams_keynames` & @`guthscp.get_team_keyname`
+]]
 local teams_keynames, keynames_teams
 function guthscp.cache_teams_keynames() 
     teams_keynames = {}
@@ -41,6 +58,23 @@ function guthscp.cache_teams_keynames()
 end
 hook.Add( "InitPostEntity", "guthscp:cache_teams_keynames", guthscp.cache_teams_keynames )
 
+--[[ 
+    @function guthscp.get_teams_keynames
+        | description: get all teams IDs mapped by their global Lua variable names
+        | example:
+            code:
+            ```lua
+            PrintTable( guthscp.get_teams_keynames() )
+            ```
+
+            output:
+            ```
+            TEAM_CITIZEN   =   1
+            TEAM_SCP096    =   2
+            ...
+            ```
+        | return: <table[string, number]> teams_keynames
+]]
 function guthscp.get_teams_keynames()
     if not teams_keynames then 
         guthscp.cache_teams_keynames() 
@@ -49,6 +83,13 @@ function guthscp.get_teams_keynames()
     return teams_keynames
 end
 
+--[[ 
+    @function guthscp.get_team_keyname
+        | description: get global Lua variable name of a given team ID
+        | params:
+            team_id: <number>
+        | return: <string> team_keyname
+]]
 function guthscp.get_team_keyname( team_id )
     if not keynames_teams then 
         guthscp.cache_teams_keynames() 
