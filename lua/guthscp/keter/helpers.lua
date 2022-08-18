0a4a153a073de8f64cd8fa943b93fea877ce11e2
Guthen <33220603+Guthen@users.noreply.github.com>
2022-08-18 15:26:56 +0200

    [added] full support of pre-release version check

--- lua/guthscp/keter/helpers.lua
+++ lua/guthscp/keter/helpers.lua
@@ -11,6 +11,7 @@ function guthscp.helpers.split_version( version )
 	return version:match( "(%d+)%.(%d+)%.(%d+)%-?(.*)" )
 end
 
+local identifier_pattern = "[%w_]+"
 function guthscp.helpers.compare_versions( current_version, extern_version )
 	local current_versions = { guthscp.helpers.split_version( current_version ) }
 	local extern_versions = { guthscp.helpers.split_version( extern_version ) }
@@ -31,17 +32,54 @@ function guthscp.helpers.compare_versions( current_version, extern_version )
 		end
 	end
 
-	--  check developpement tag
+	--  check pre-release tag
 	local current = current_versions[4]
-	if #current > 0 then
-		local extern = extern_versions[4]
-
-		--  assuming that a version without a tag means a production version 
-		if #extern == 0 then
+	local extern = extern_versions[4]
+	if #current > 0 or #extern > 0 then
+		if #current == 0 then
+			return 1, 4
+		elseif #extern == 0 then
 			return -1, 4
-		--  same tag?
-		elseif current == extern then
+		end
+
+		--  equal? no need to do fancy comparison
+		if current == extern then
 			return 0, 4
+		--  fancy comparison, let's go
+		else
+			local current_iter = current:gmatch( identifier_pattern )
+			local extern_iter = extern:gmatch( identifier_pattern )
+
+			while true do
+				local current_word = current_iter()
+				if not current_word then 
+					return -1, 4
+				end
+
+				local extern_word = extern_iter()
+				if not extern_word then 
+					return 1, 4
+				end
+
+				--  parse numbers
+				local num = tonumber( current_word )
+				if num then 
+					current_word = num
+					num = tonumber( extern_word )
+					if num then
+						extern_word = num
+					else
+						return -1, 4
+					end
+				end
+
+				--  compare
+				if current_word > extern_word then
+					return 1, 4
+				elseif extern_word > current_word then
+					return -1, 4
+				end
+			end
 		end
 	end
 
