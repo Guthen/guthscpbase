2b3b7fabc5a23e00a661a41858b7d4338de48353
Guthen <33220603+Guthen@users.noreply.github.com>
2022-09-28 11:32:37 +0200

    [#4] added cptbase workaround

---
+++ lua/guthscp/modules/base/workarounds/cptbase_shouldcollide.lua
@@ -0,0 +1,31 @@
+local WORKAROUND = {
+	name = "Fix 'CPTBase' conflicting the 'ShouldCollide' hook",
+	realm = guthscp.REALMS.SERVER,
+}
+
+function WORKAROUND:init()
+	for k, v in pairs( hook.GetTable()["ShouldCollide"] ) do
+        if k:find( "CPTBase" ) then
+			self._hook_id = k
+            self._former_callback = v
+           	return true
+        end
+    end
+
+	self:warning( "hook 'ShouldCollide' of 'CPTBase' wasn't found!" )
+	return false
+end
+
+function WORKAROUND:on_enabled()
+	hook.Add( "ShouldCollide", self._hook_id, function( ent1, ent2 )
+		if ent1:GetClass() == "cpt_ai_pathfinding" and ent2 == ent1:GetOwner() then
+			return false
+		end
+	end )
+end
+
+function WORKAROUND:on_disabled()
+	hook.Add( "ShouldCollide", self._hook_id, self._former_callback )
+end
+
+guthscp.workaround.register( "cptbase_shouldcollide", WORKAROUND )
