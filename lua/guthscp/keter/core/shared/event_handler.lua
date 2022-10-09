162d54629d53f15bd6b90aa88843a219a7c3a748
Guthen <33220603+Guthen@users.noreply.github.com>
2022-10-09 21:59:30 +0200

    [added] event handler class

---
+++ lua/guthscp/keter/core/shared/event_handler.lua
@@ -0,0 +1,53 @@
+guthscp.event_handler = guthscp.event_handler or {}
+
+local HANDLER = guthscp.event_handler
+HANDLER.__index = HANDLER
+
+--  new
+function HANDLER:new()
+	local obj = {
+		listeners_count = 0,
+		listeners = {},
+	}
+	
+	return setmetatable( obj, HANDLER )
+end
+
+
+--  setters
+function HANDLER:add_listener( id, callback )
+	if self.listeners[id] then return end
+
+	self.listeners[id] = callback
+	self.listeners_count = self.listeners_count + 1
+end
+
+function HANDLER:remove_listener( id )
+	if not self.listeners[id] then return end
+
+	self.listeners[id] = nil
+	self.listeners_count = self.listeners_count - 1
+end
+
+function HANDLER:clear()
+	self.listeners = {}
+	self.listeners_count = 0
+end
+
+
+--  invoke
+function HANDLER:invoke( ... )
+	for id, callback in pairs( self.listeners ) do
+		callback( ... )
+	end
+end
+
+
+--  getters
+function HANDLER:get_listeners()
+	return self.listeners
+end
+
+function HANDLER:get_listeners_count()
+	return self.listeners_count
+end
--- lua/guthscp/modules/base/unit_test.lua
+++ lua/guthscp/modules/base/unit_test.lua
@@ -2,11 +2,17 @@ local MODULE = guthscp.modules.base
 
 guthscp.print_tabs = guthscp.print_tabs + 1
 
+local function unit_testing( name, callback )
+	MODULE:info( name )
+
+	guthscp.print_tabs = guthscp.print_tabs + 1
+	callback()
+	guthscp.print_tabs = guthscp.print_tabs - 1
+end
+
 --  guthscp.helpers.compare_versions( current_version, extern_version )
 --  see https://semver.org/
-MODULE:info( "guthscp.helpers.compare_versions" )
-guthscp.print_tabs = guthscp.print_tabs + 1
-do 
+unit_testing( "guthscp.helpers.compare_versions", function()
 	--  in acsending order
 	local tests = {
 		"1.0.0-alpha",
@@ -48,7 +54,39 @@ do
 
 		current = previous
 	end
-end
-guthscp.print_tabs = guthscp.print_tabs - 1
+end )
+
+
+unit_testing( "guthscp.event_handler", function()
+	local handler = guthscp.event_handler:new()
+	
+	handler:add_listener( "#1", function( ... )
+		MODULE:info( "#1: %s", table.concat( { ... }, "; " ) )
+		handler:remove_listener( "#1" )
+	end )
+	handler:add_listener( "#2", function( ... )
+		MODULE:info( "#2: %s", table.concat( { ... }, "; " ) )
+		handler:remove_listener( "#2" )
+	end )
+	handler:add_listener( "#3", function( ... )
+		MODULE:info( "#3: %s", table.concat( { ... }, "; " ) )
+		--handler:remove_listener( "#3" )
+	end )
+
+	MODULE:info( "first invoke:" )
+	guthscp.print_tabs = guthscp.print_tabs + 1
+	handler:invoke( 1, "hey are you okay?", "no" )
+	guthscp.print_tabs = guthscp.print_tabs - 1
+
+	MODULE:info( "second invoke:" )
+	guthscp.print_tabs = guthscp.print_tabs + 1
+	handler:invoke( 2, "hey are you still okay?", "no" )
+	guthscp.print_tabs = guthscp.print_tabs - 1
+
+	MODULE:info( "third invoke:" )
+	guthscp.print_tabs = guthscp.print_tabs + 1
+	handler:invoke( 3, "and now??", "yeah" )
+	guthscp.print_tabs = guthscp.print_tabs - 1
+end )
 
 guthscp.print_tabs = guthscp.print_tabs - 1
