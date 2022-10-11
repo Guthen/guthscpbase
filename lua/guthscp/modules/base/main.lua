1a7dcc074437eb0f5dba1694df1445e10b2a319b
Guthen <33220603+Guthen@users.noreply.github.com>
2022-10-11 13:45:05 +0200

    [edited] base module description

--- lua/guthscp/modules/base/main.lua
+++ lua/guthscp/modules/base/main.lua
@@ -8,6 +8,7 @@ It comes with everything considered useful for making SCPs addons work together 
 The base allows easy module creation with their own in-game configuration usable anywhere in the code.
 It also includes:
 ─ its own spawnmenu for referencing all SCPs Weapons, Entities and the Configurations
+─ a workaround system for fixing conflicts & issues with external addons
 ─ a custom shared sound system for easily playing looping and 3D-spatialized sounds
 ─ an entity breaking system, useful for throwing doors and chairs at your victims while playing as SCP-096
 ─ some useful functions for managing file data, getting a list of living NPCs, manipulating Lua tables..]],
