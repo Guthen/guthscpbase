057dc1897df5870fa8a874793ef44f49a16b076d
Guthen <33220603+Guthen@users.noreply.github.com>
2023-07-24 22:11:35 +0200

    [edited] players filter `listen_` are now server methods

--- lua/guthscp/keter/core/shared/filters/players_filter.lua
+++ lua/guthscp/keter/core/shared/filters/players_filter.lua
@@ -10,30 +10,32 @@ function FILTER:filter( ent )
 end
 
 --  hook listeners
-function FILTER:listen_weapon_users( weapon_class )
-	hook.Add( "WeaponEquip", self.global_id, function( weapon, ply )
-		if not ( weapon:GetClass() == weapon_class ) then return end
+if SERVER then
+	function FILTER:listen_weapon_users( weapon_class )
+		hook.Add( "WeaponEquip", self.global_id, function( weapon, ply )
+			if not ( weapon:GetClass() == weapon_class ) then return end
+			
+			self:add( ply )
+		end )
+		hook.Add( "PlayerDroppedWeapon", self.global_id, function( ply, weapon )
+			if not ( weapon:GetClass() == weapon_class ) then return end
+			
+			self:remove( ply )
+		end )
 		
-		self:add( ply )
-	end )
-	hook.Add( "PlayerDroppedWeapon", self.global_id, function( ply, weapon )
-		if not ( weapon:GetClass() == weapon_class ) then return end
-		
-		self:remove( ply )
-	end )
+		local function remove_if_not_has_weapon( ply )
+			if not ply:HasWeapon( weapon_class ) then return end
 	
-	local function remove_if_not_has_weapon( ply )
-		if not ply:HasWeapon( weapon_class ) then return end
-
-		self:remove( ply )
+			self:remove( ply )
+		end
+		hook.Add( "DoPlayerDeath", self.global_id, remove_if_not_has_weapon )
+		hook.Add( "PlayerSilentDeath", self.global_id, remove_if_not_has_weapon )
+		hook.Add( "OnPlayerChangedTeam", self.global_id, remove_if_not_has_weapon )
 	end
-	hook.Add( "DoPlayerDeath", self.global_id, remove_if_not_has_weapon )
-	hook.Add( "PlayerSilentDeath", self.global_id, remove_if_not_has_weapon )
-	hook.Add( "OnPlayerChangedTeam", self.global_id, remove_if_not_has_weapon )
-end
-
-function FILTER:listen_disconnect()
-	hook.Add( "PlayerDisconnected", self.global_id, function( ply )
-		self:remove( ply )
-	end )
+	
+	function FILTER:listen_disconnect()
+		hook.Add( "PlayerDisconnected", self.global_id, function( ply )
+			self:remove( ply )
+		end )
+	end
 end
